8081b819146599cc00ef389ba7a3caa50ba33e65
utkin-apex <ootiach@gmail.com>
2016-01-21 13:35:42 +0300

    Добавил карточки клиента в Дашборд

---
+++ js/controllers/MainController.js
@@ -0,0 +1,20 @@
+app.controller('MainController', ['$scope', function($scope) {
+
+  $scope.clientcards = [
+  	{
+    	name: 'Аррива',
+
+  	},
+      {
+    	name: 'Летний сад',
+
+  	},
+      {
+    	name: 'Синдик',
+
+  	},
+
+  ];
+
+
+}]);
