8b24f0f3ec0f0b1b0af3349412b95e03579a645a
utkin-apex <ootiach@gmail.com>
2016-01-21 18:09:31 +0300

    Добавлены переключатели визит-лид-сделка-доход

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,20 +1,41 @@
-app.controller('MainController', ['$scope', function($scope) {
+app.controller('MainController', ['$scope', function ($scope) {
 
-  $scope.clientcards = [
-  	{
-    	name: 'Аррива',
+	$scope.clientcards = [
+		{
+			name: 'Аррива',
+			visits: 15,
+			lids: 5,
+			sales: 2,
+			income: 55000,
 
   	},
-      {
-    	name: 'Летний сад',
+		{
+			name: 'Летний сад',
+			visits: 35,
+			lids: 12,
+			sales: 4,
+			income: 123000,
 
   	},
-      {
-    	name: 'Синдик',
+		{
+			name: 'Синдик',
+			visits: 20,
+			lids: 8,
+			sales: 1,
+			income: 8000,
 
   	},
 
   ];
 
+
+
+
+	$scope.param = "visits";
+
+	$scope.plus = function(index,param) {
+    return this.clientcards[index][param];
+
+	};
 
 }]);
