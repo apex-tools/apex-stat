fe546a9f873d9ca5031af96f627521807ca6fdbc
Илья <>
2016-01-26 00:17:41 +0300

    добавлены кнопки, вычешены padding у таблиц, добавлена ширина ячеек. Есть проблемка! таблицы не хотят равняться по ширине ячеек

--- js/app.js
+++ js/app.js
@@ -9,15 +9,17 @@ $.getScript('http://cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.0/morris.min.js
   Morris.Line({
         element: 'line-example',
         data: [
-          {year: '2010', value: 20, value2: 15},
-          {year: '2011', value: 10, value2: 12},
-          {year: '2012', value: 5, value2: 4},
-          {year: '2013', value: 2, value2: 5},
-          {year: '2014', value: 20, value2: 26}
+          {x: '2015-01-01', visits: 203, lids: 21, sales: 11},
+          {x: '2015-01-02', visits: 210, lids: 21, sales: 7},
+          {x: '2015-01-03', visits: 215, lids: 20, sales: 1},
+          {x: '2015-01-04', visits: 223, lids: 22, sales: 2},
+          {x: '2015-01-05', visits: 206, lids: 26, sales: 12},
+			{x: '2015-01-06', visits: 115, lids: 13, sales: 6},
+			{x: '2015-01-07', visits: 92, lids: 8, sales: 2}
         ],
-        xkey: 'year',
-        ykeys: ['value','value2'],
-        labels: ['Value','value2']
+        xkey: 'x',
+        ykeys: ['visits','lids','sales'],
+        labels: ['Визиты','Заявки','Продажи']
       });
 
 
